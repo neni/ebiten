001042b2c1c2e7ef2864528417bdc65ca8c8177e
stef <dev@neni.org>
2022-01-11 08:19:44 +0100

    add ebitenutil.NewImageFromFileSystem()

--- ebitenutil/loadimagefs.go
+++ ebitenutil/loadimagefs.go
@@ -12,29 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !android && !ios
-// +build !android,!ios
-
 package ebitenutil
 
 import (
-	"image"
 	"io"
+	"io/fs"
+	"image"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-// NewImageFromFile loads the file with path and returns ebiten.Image and image.Image.
+// NewImageFromFileSystem loads the file from fs.FS with path and returns ebiten.Image and image.Image.
 //
-// Image decoders must be imported when using NewImageFromFile. For example,
+// Image decoders must be imported when using NewImageFromFileSystem. For example,
 // if you want to load a PNG image, you'd need to add `_ "image/png"` to the import section.
-//
-// How to solve path depends on your environment. This varies on your desktop or web browser.
-// Note that this doesn't work on mobiles.
-//
-// For productions, instead of using NewImageFromFile, it is safer to embed your resources with go:embed.
-func NewImageFromFile(path string) (*ebiten.Image, image.Image, error) {
-	file, err := OpenFile(path)
+func NewImageFromFileSystem(fsFs fs.FS, path string) (*ebiten.Image, image.Image, error) {
+	file, err := fsFs.Open(path)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -44,3 +37,15 @@ func NewImageFromFile(path string) (*ebiten.Image, image.Image, error) {
 	return NewImageFromReader(file)
 }
 
+// NewImageFromReader loads from the io.Reader and returns ebiten.Image and image.Image.
+//
+// Image decoders must be imported when using NewImageFromReader. For example,
+// if you want to load a PNG image, you'd need to add `_ "image/png"` to the import section.
+func NewImageFromReader(reader io.Reader) (*ebiten.Image, image.Image, error) {
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		return nil, nil, err
+	}
+	img2 := ebiten.NewImageFromImage(img)
+	return img2, img, err
+}
